5cfe2b5e5482301b73c3e82c621c736fc9e99c98
Tom <tom@dinosaur.co.uk>
2016-04-29 17:30:12 +0100

    New design, added work view page

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+
+
+// Show menu when click burger
 $('.burger').on('click', function(){
     $('#big-menu').toggle('slow');
 });
 
 var box;
 
+// Vertically center content of work page boxes
 $('.gridbox').each(function(box){
     var thisHeight = $(this).height() / 2;
     var kidHeight = $(this).children('.content').height() /2;
@@ -11,5 +15,26 @@ $('.gridbox').each(function(box){
     $(this).children('.content').css('margin-top', marginTop);
 });
 
-var picHeight = $('#banner .content').children('img').height() - 15;
+
+// Vertically center content of banner
+var picHeight = $('#banner .content').children('.banner-img').height() - 15;
+var bannerTextHeight = $('.banner-text').children('.banner-text-container').height();
+var bannerTextTop = (picHeight/2) - bannerTextHeight;
+
 $('#banner').height(picHeight);
+$('.banner-text').height(picHeight);
+$('.banner-text').children('.banner-text-container').css('margin-top', bannerTextTop)
+
+
+$(window).resize(function(){
+    // Vertically center content of banner
+    var picHeight = $('#banner .content').children('.banner-img').height() - 15;
+    var bannerTextHeight = $('.banner-text').children('.banner-text-container').height();
+    var bannerTextTop = (picHeight/2) - bannerTextHeight;
+
+    $('#banner').height(picHeight);
+    $('.banner-text').height(picHeight);
+    $('.banner-text').children('.banner-text-container').css('margin-top', bannerTextTop)
+
+
+});
