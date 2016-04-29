24651525586afd6eff7c4120f46270b17e9bc25f
Tom <tom@dinosaur.co.uk>
2016-04-29 10:03:17 +0100

    Wireframes for the website

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 $('.burger').on('click', function(){
     $('#big-menu').toggle('slow');
 });
+
+var box;
+
+$('.gridbox').each(function(box){
+    var thisHeight = $(this).height() / 2;
+    var kidHeight = $(this).children('.content').height() /2;
+    var marginTop = thisHeight-kidHeight+'px';
+    $(this).children('.content').css('margin-top', marginTop);
+});
+
+var picHeight = $('#banner .content').children('img').height() - 15;
+$('#banner').height(picHeight);
